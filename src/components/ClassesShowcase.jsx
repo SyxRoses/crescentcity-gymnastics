991216556a495168b6c1af79aaa93fb86e15618f
Chaz <czyn@Chazs-MBP.lan>
2025-12-27 13:26:07 -0600

    Fixed the Learn More buttons so that they redirect to the pricingpage

--- src/components/ClassesShowcase.jsx
+++ src/components/ClassesShowcase.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './ClassesShowcase.css';
 
 function ClassesShowcase() {
@@ -118,12 +119,12 @@ function ClassesShowcase() {
                                     <span className="class-price">{classItem.price}</span>
                                 </div>
                                 <p>{classItem.description}</p>
-                                <a href={`#${classItem.id}`} className="class-card-link">
+                                <Link to="/pricing" className="class-card-link">
                                     Learn More
                                     <svg viewBox="0 0 20 20" fill="currentColor" width="16" height="16">
                                         <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
                                     </svg>
-                                </a>
+                                </Link>
                             </div>
                         </article>
                     ))}
